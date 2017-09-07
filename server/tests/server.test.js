808d8e56e10c37a0193cd36aa22931e15f3a2a43
retief <retief.visser@fivium.com.au>
2017-09-07 18:02:43 +1000

    made first half of routes private

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -15,6 +15,7 @@ describe('POST / todos', () => {
     var text = 'Test todo text';
     request(app)
       .post('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .send({ text })
       .expect(200)
       .expect(res => {
@@ -36,6 +37,7 @@ describe('POST / todos', () => {
   it('should not create todo with invalid body data', done => {
     request(app)
       .post('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .send({})
       .expect(400)
       .end((err, res) => {
@@ -55,9 +57,10 @@ describe('GET /todos', () => {
   it('should get all todos', (done) => {
     request(app)
       .get('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .expect(200)
       .expect(res => {
-        expect(res.body.todos.length).toBe(2);
+        expect(res.body.todos.length).toBe(1);
       })
       .end(done);
   });
@@ -308,7 +311,7 @@ describe('DELETE /users/me/token', () => {
 
         User.findById(users[0]._id).then(user => {
           expect(user.tokens.length).toBe(0);
-          done()
+          done();
         }).catch(e => done(e));
       });
   });
